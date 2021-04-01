d5410c13628fb1c522854cfbf48ec8a60b89aba9
Пётр Васасов <Stepa199910@mail.ru>
2021-04-01 10:59:40 +0700

    Созданы необходимые проекты, начал реализовывать проект параметров

---
+++ CompasConnector/CompassConnector.cs
@@ -0,0 +1,115 @@
+﻿using Kompas6API5;
+using Kompas6Constants3D;
+using System;
+using System.Runtime.InteropServices;
+
+namespace CompassConnector
+{
+    /// <summary>
+    /// Класс для соединения с Компас 3D
+    /// </summary>
+    public class CompassConnector
+    {
+        /// <summary>
+        /// Поле, хранящее данные о 3D детали
+        /// </summary>
+        private ksDocument3D _document3d;
+
+        /// <summary>
+        /// Поле предназназначено для связи с API Компас 
+        /// </summary>
+        private KompasObject _compassObject;
+
+        /// <summary>
+        /// Поле для создание интерфейса
+        /// </summary>
+        private static CompassConnector _instance;
+
+        /// <summary>
+        /// Поле, для работы с частями детали 
+        /// </summary>
+        private ksPart _compassPart;
+
+        /// <summary>
+        /// Свойство для реализации интерфейса API Компас 
+        /// </summary>
+        public static CompassConnector Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new CompassConnector();
+                //_instance.InitializationKompas();
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// Свойство для реализации связи с API Компас
+        /// </summary>
+        public KompasObject CompassObject
+        {
+            get { return _compassObject; }
+            set { _compassObject = value; }
+        }
+
+        /// <summary>
+        /// Сойство для реализации связи с частями детали
+        /// </summary>
+        public ksPart CompassPart
+        {
+            get { return _compassPart; }
+            set { _compassPart = value; }
+        }
+
+        /// <summary>
+        /// Сойство для реализации связи с 3D эскизом
+        /// </summary>
+        public ksDocument3D Document3D
+        {
+            get { return _document3d; }
+            set { _document3d = value; }
+        }
+
+        /// <summary>
+        /// Метод запуска Компас в режиме детали, инициализации свойств Document3D, CompassPart и CompassObject
+        /// </summary>
+        public void InitializationKompas()
+        {
+            if (_compassObject == null)
+            {
+#if __LIGHT_VERSION__
+                Type t = Type.GetTypeFromProgID("KOMPASLT.Application.5");
+#else
+                Type t = Type.GetTypeFromProgID("KOMPAS.Application.5");
+#endif
+                _compassObject = (KompasObject)Activator.CreateInstance(t);
+                _document3d = (Document3D)_compassObject.Document3D();
+                _document3d.Create(false, true);
+                _compassPart = (ksPart)_document3d.GetPart((short)Part_Type.pTop_Part);
+            }
+            if (_compassObject != null)
+            {
+                _compassObject.Visible = true;
+                _compassObject.ActivateControllerAPI();
+            }
+        }
+
+        /// <summary>
+        /// Метод для выгрузки и выхода и компаса
+        /// </summary>
+        public void UnloadKompas()
+        {
+            if (_compassObject != null)
+            {
+                _compassObject.Quit();
+                Marshal.ReleaseComObject(_compassObject);
+            }
+        }
+
+        /// <summary>
+        /// Конструктор класса
+        /// </summary>
+        private CompassConnector() { }
+    }
+}
---
+++ NghtstandParameters/NightstandParameters.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NightstandParameters
+{
+    public class NightstandParameters
+    {
+        /// <summary>
+        /// Поле, содержащее высоту столешницы 
+        /// </summary>
+        private Parameter _topHeight;
+
+        /// <summary>
+        /// Поле, содержащее ширину столешницы
+        /// </summary>
+        private Parameter _topWidth;
+
+        /// <summary>
+        /// Поле, содержащее глубину столешницы
+        /// </summary>
+        private Parameter _topDeep;
+
+        /// <summary>
+        /// Поле, содержащее высота "туловища"
+        /// </summary>
+        private Parameter _bodyHeight;
+
+        /// <summary>
+        /// Поле, содержащее ширину "туловища"
+        /// </summary>
+        private Parameter _bodyWidth;
+
+        /// <summary>
+        /// Поле, содержащее глубину "туловища"
+        /// </summary>
+        private Parameter _bodyDeep;
+
+        /// <summary>
+        /// Поле, содержащее высоту ящика
+        /// </summary>
+        private Parameter _boxHeight;
+
+        /// <summary>
+        /// Поле, содержащее ширину ящика
+        /// </summary>
+        private Parameter _boxWidth;
+
+        /// <summary>
+        /// Поле, содержащее длину ножек
+        /// </summary>
+        private Parameter _footLength;
+
+        public Parameter TopHeight
+        {
+            get => _topHeight;
+            set
+            {
+                _topHeight = value;
+            }
+        }
+
+        public Parameter TopWidth
+        {
+            get => _topWidth;
+            set
+            {
+                _topWidth = value;
+            }
+        }
+
+        public Parameter TopDeep
+        {
+            get => _topDeep;
+            set
+            {
+                _topDeep = value;
+            }
+        }
+
+        public Parameter BodyHeight
+        {
+            get => _bodyHeight;
+            set
+            {
+                _bodyHeight = value;
+            }
+        }
+        public Parameter BodyWidth
+        {
+            get => _bodyWidth;
+            set
+            {
+                _bodyWidth = value;
+            }
+        }
+        public Parameter BodyDeep
+        {
+            get => _bodyDeep;
+            set
+            {
+                _bodyDeep = value;
+            }
+        }
+        public Parameter BoxHeight
+        {
+            get => _boxHeight;
+            set
+            {
+                _boxHeight = value;
+            }
+        }
+        public Parameter BoxWidth
+        {
+            get => _boxWidth;
+            set
+            {
+                _boxWidth = value;
+            }
+        }
+        public Parameter FootLength
+        {
+            get => _footLength;
+            set
+            {
+                _footLength = value;
+            }
+        }
+
+        public void MaxValue()
+        {
+            _bodyDeep.Value = _bodyDeep.MaximumValue;
+            _topHeight.Value = _topDeep.MaximumValue;
+            _footLength.Value = _footLength.MaximumValue;
+            _boxWidth.Value = _boxWidth.MaximumValue;
+            _boxHeight.Value = _boxHeight.MaximumValue;
+            _bodyWidth.Value = _bodyWidth.MaximumValue;
+            _topWidth.Value = _topWidth.MaximumValue;
+            _bodyHeight.Value = _bodyHeight.MaximumValue;
+            _topDeep.Value = _topDeep.MaximumValue;
+        }
+        public void MinValue()
+        {
+            _bodyDeep.Value = _bodyDeep.MinimumValue;
+            _topHeight.Value = _topDeep.MinimumValue;
+            _footLength.Value = _footLength.MinimumValue;
+            _boxWidth.Value = _boxWidth.MinimumValue;
+            _boxHeight.Value = _boxHeight.MinimumValue;
+            _bodyWidth.Value = _bodyWidth.MinimumValue;
+            _topWidth.Value = _topWidth.MinimumValue;
+            _bodyHeight.Value = _bodyHeight.MinimumValue;
+            _topDeep.Value = _topDeep.MinimumValue;
+        }
+    }
+}
